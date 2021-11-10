379266fda4471331b8edcdbaca71ee41c252d518
Sangwoo <vlclak12@gmail.com>
2021-11-10 18:49:33 +0900

    똥피하기 퀴즈

--- pygame_basic/quiz.py
+++ pygame_basic/quiz.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 ##############################################################
 # 기본 초기화 (반드시 해야 하는 것들)
@@ -31,9 +32,14 @@ character_y_pos = screen_height - character_height
 to_x = 0
 character_speed = 10
 
-
-enemy = pygame.image.load("D:/song/python/Workspace/Python_game_1/pygame_basic/enemy.png")
-
+# 똥 만들기
+ddong = pygame.image.load("D:/song/python/Workspace/Python_game_1/pygame_basic/enemy.png")
+ddong_size = ddong.get_rect().size
+ddong_width = ddong_size[0]
+ddong_height = ddong_size[1]
+ddong_x_pos = random.randint(0, screen_width - ddong_width)
+ddong_y_pos = 0
+ddong_speed = 20
 
 
 running = True
@@ -63,12 +69,29 @@ while running:
         character_x_pos = 0
     elif character_x_pos > screen_width - character_width:
         character_x_pos = screen_width - character_width
-    
+
+    ddong_y_pos += ddong_speed
+    if ddong_y_pos > screen_height:
+        ddong_y_pos = 0
+        ddong_x_pos = random.randint(0, screen_width - ddong_width)
+
     # 4. 충돌 처리
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
+
+    ddong_rect = ddong.get_rect()
+    ddong_rect.left = ddong_x_pos
+    ddong_rect.top = ddong_y_pos
+
+    if character_rect.colliderect(ddong_rect):
+        print("충돌했어요")
+        running = False
 
     # 5. 화면에 그리기
     screen.blit(background, (0,0))
     screen.blit(character, (character_x_pos, character_y_pos))
+    screen.blit(ddong,(ddong_x_pos, ddong_y_pos))
     
     pygame.display.update()
 
